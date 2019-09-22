60d898ad8bee9ff32d97ea5a73a4d162c4d8df96
chenwenqiang <chenwq@hxdi.com>
2019-09-22 11:23:38 +0800

    修改签名

--- src/main/java/com/atguigu/git/test/TestGit.java
+++ src/main/java/com/atguigu/git/test/TestGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test git");
+        System.out.println("test idea git");
     }
 }

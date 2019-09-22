849845b875deec36c26f5969bd624731d5c2ba86
chenwq <chenwq@hxdi.com>
2019-09-22 17:22:53 +0800

    test new Branch

--- src/main/java/com/atguigu/git/test/TestNewBranch.java
+++ src/main/java/com/atguigu/git/test/TestNewBranch.java
@@ -3,5 +3,6 @@ package com.atguigu.git.test;
 public class TestNewBranch {
     public static void main(String[] args) {
         System.out.println("test new branch");
+        System.out.println("test");
     }
 }

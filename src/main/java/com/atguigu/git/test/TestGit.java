7c4ae6a408e8fe4f543f081c8f676ecf3006c3d4
chenwq <54857729+lawrence-cwq@users.noreply.github.com>
2019-09-22 16:28:41 +0800

    Update TestGit.java

--- src/main/java/com/atguigu/git/test/TestGit.java
+++ src/main/java/com/atguigu/git/test/TestGit.java
@@ -4,6 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("test git");
         System.out.println("test idea git");
-	System.out.println("test");
+	System.out.println("test 111");
     }
 }
